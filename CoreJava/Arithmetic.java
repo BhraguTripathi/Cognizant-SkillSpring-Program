e318d6ce6bb993e93c53afc533173ca25f443acc
BhraguTripathi <bhragu.12324742@lpu.in>
2026-06-25 14:48:42 +0530

    Java Practice

---
+++ CoreJava/Arithmetic.java
@@ -0,0 +1,10 @@
+package CoreJava;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a = -3;
+        int b = 4;
+        System.out.println(a%b);
+        System.out.println(a+b);
+    }
+}
