420b3d94949d6b31ee6f4437ef53f25452c71e9c
BhraguTripathi <bhragu.12324742@lpu.in>
2026-06-26 15:49:35 +0530

    Java Practice

---
+++ CoreJava/Arrays.java
@@ -0,0 +1,10 @@
+package CoreJava;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int [] arr ={4,5,6,7,8};
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+    }
+}
