cde81a94c75890bbdb5186516362e4104111ffc5
BhraguTripathi <bhragu.12324742@lpu.in>
2026-06-26 12:38:00 +0530

    Java Practice

---
+++ CoreJava/Classes_Object.java
@@ -0,0 +1,19 @@
+package CoreJava;
+
+class Calculator {   // class to perform basic add operation
+    public int add (int a, int b){   // method to add two numbers in the calculator class
+        return a+b;
+    }
+
+}
+
+public class Classes_Object {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        Calculator calc = new Calculator();  // creating object of Calculator class
+        int sum = calc.add(num1, num2);
+        System.out.println("Sum: " + sum);
+    }
+}
---
+++ CoreJava/MethodOverloading.java
@@ -0,0 +1,18 @@
+package CoreJava;
+
+class Calculators{
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public double add(double a, double b){
+        return a+b;
+    }
+}
+public class MethodOverloading {
+    public static void main(String[] args) {
+        Calculators calc = new Calculators();
+        System.out.println(calc.add(10.0,20.0));
+        System.out.println(calc.add(10,20));
+    }
+}
