2ab95d64704daf7e0ed46ef1016f2e5f4d0030a3
BhraguTripathi <bhragu.12324742@lpu.in>
2026-06-26 09:07:55 +0530

    Java Practice

---
+++ CoreJava/ConditionalStatement.java
@@ -0,0 +1,20 @@
+package CoreJava;
+
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        if (a > b) {
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("a is less than or equal to b");
+        }
+
+        int i=1;
+        switch(i){
+            case 1: System.out.println("i is 1"); break;
+            case 2:System.out.println("i is 2"); break;
+            default: System.out.println("i is neither 1 nor 2");break;
+        }
+    }
+}
